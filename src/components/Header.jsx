91f51e62b49041b79214f8f01b5015e29f0f075d
Richard Crespo <rjcrespo96@gmail.com>
2022-02-26 18:05:46 -0500

    [edit] header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <>
-      <h1 className='font-black text-5xl text-center md:w-1/2 mx-auto'>Seguimiento pacientes{" "} <br />
+      <h1 className='font-black text-5xl text-center md:w-1/2 mx-auto'>Seguimiento pacientes{" "} con Github <br />
         <span className='text-indigo-600'>Veterinaria</span>
       </h1>
     </>
